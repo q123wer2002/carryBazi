68777d60e0f792e59b91659f75fb6c66a3faae40
chpplen <chpplen@hotmail.com>
2016-02-14 14:03:54 +0800

    theme page

---
+++ theme.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once "include/baziMain.php";
+
+$BPhotoObj->topHtml = "top.html";
+$BPhotoObj->contentHtml = "content/theme.html";
+$BPhotoObj->footerHtml = "footer.html";
+
+$BPhotoObj->showHTML("masterTheme.html");
+
+?>
