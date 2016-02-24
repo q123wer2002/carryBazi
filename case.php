7b4a2c59029993211305db728b847fbf6bf160e4
chpplen <chpplen@hotmail.com>
2016-02-24 22:34:09 +0800

    case page

---
+++ case.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once "include/baziMain.php";
+
+$BPhotoObj->topHtml = "top.html";
+$BPhotoObj->contentHtml = "content/case.html";
+$BPhotoObj->footerHtml = "footer.html";
+
+$BPhotoObj->showHTML("masterCase.html");
+
+?>
