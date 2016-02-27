24f2f65d76a030390c6d02472d1e68293e0da456
unknown <q123wer2002@gmail.com>
2016-02-28 00:00:04 +0800

    adjust top page

--- include/baziMain.php
+++ include/baziMain.php
@@ -7,4 +7,8 @@ header("Content-Type:text/html; charset=utf-8");
 
 $BPhotoObj = new BaziPhoto();
 
+$BPhotoObj->metaHtml = "meta.html";
+$BPhotoObj->topHtml = "top.html";
+$BPhotoObj->footerHtml = "footer.html";
+
 ?>
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 include_once "include/baziMain.php";
 
+
 $BPhotoObj->topHtml = "top.html";
 $BPhotoObj->contentHtml = "content/home.html";
 $BPhotoObj->footerHtml = "footer.html";
-
 $BPhotoObj->showHTML("index.html");
 
 ?>
