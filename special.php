744ba121282610365afde5f4f337ea29e3c0de01
chpplen <chpplen@hotmail.com>
2016-03-13 17:26:30 +0800

    special page

---
+++ special.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once "include/baziMain.php";
+
+$BPhotoObj->topHtml = "top.html";
+$BPhotoObj->contentHtml = "content/special.html";
+$BPhotoObj->footerHtml = "footer.html";
+
+$BPhotoObj->showHTML("masterTheme.html");
+
+?>
