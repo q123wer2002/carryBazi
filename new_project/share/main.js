0dc736056fea1fc38caf130768a91aa9d05a4988
Danny <q123wer2002@gmail.com>
2016-07-02 14:13:23 +0800

    upload

--- new_project/share/main.js
+++ new_project/share/main.js
@@ -1,58 +1,13 @@
-var CarryBaziPhoto = angular.module('CarryBaziPhoto',['ngRoute', 'luegg.directives']);
-
-CarryBaziPhoto.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/',{
-    })
-    .when('/search', {
-    templateUrl : 'templates/web/content/search.html',
-    });
-}]);
-
-CarryBaziPhoto.controller('MainController',['$scope','$http', '$interval', '$route', '$routeParams', '$location',function SyntecRemote($scope,$http,$interval,$route,$routeParams,$location){
-    this.$route = $route;
-    this.$location = $location;
-    this.$routeParams = $routeParams;
-
-    //template url
-    $scope.templates = {
-        TOP    : "share/top.html",
-        BODY   : "",
-        FOOTER : "share/footer.html",
-    };
-
-
-}]);
-
-CarryBaziPhoto.filter('round', function() {
-    return function(input) {
-        return Math.round(input*100)/100;
-    };
+require.config({
+    baseUrl: '../new_project',
+    urlArgs: 'v=1.0'
 });
 
-//right click
-CarryBaziPhoto.directive('ngRightClick', function($parse) {
-    return function(scope, element, attrs) {
-        var fn = $parse(attrs.ngRightClick);
-        element.bind('contextmenu', function(event) {
-            scope.$apply(function() {
-                event.preventDefault();
-                fn(scope, {$event:event});
-            });
-        });
-    };
-});
-
-//press enter
-CarryBaziPhoto.directive('ngEnter', function() {
-    return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
-            if(event.which === 13) {
-                scope.$apply(function(){
-                        scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
-});
+require([
+        'share/services/routeResolver',
+        'share/app'
+    ], function() {
+	//do nothing
+		angular.module(document, ['CarryBaziPhoto']);
+	}
+);
